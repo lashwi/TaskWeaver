34a28c435adc46f299ace8a84bca61adebffdbb6
lashwi <alamani2@illinois.edu>
2023-11-10 17:00:36 -0600

    view dependency graph, make input field as multiselect in task details page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import Image from 'next/image'
 import React from "react";
-import GridCanvas from '@/components/Canvas';
-import Task from '@/components/Task';
 import TaskDetails from '@/components/TaskDetails';
 import { useState } from 'react';
 
@@ -20,11 +17,6 @@ export default function Home() {
 
   return (
     <div style={{ background: 'white' }}>
-      {/* <Task title="Hello" details="Details maybe" /> */}
-      {/* <Task text="Task 2" details="More details" /> */}
-      {/*<TaskDetails />*/}
-      <h1 style={{ color: 'black' }}>Grid Canvas Example</h1>
-      <GridCanvas />
       <div>
         {/* <TaskDetails /> */}
         {/* Button to open/close the TaskPopup */}
