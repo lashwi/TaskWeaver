72be17c73b4bd0544082b1796fd785c0121481f1
Minh Duong <git@whitehoodhacker.net>
2023-11-10 13:30:03 -0600

    Add WIP draggable task

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,11 +3,25 @@ import React from "react";
 import GridCanvas from '@/components/Canvas';
 import Task from '@/components/Task';
 
+const tasks = [
+  {
+    id: 1,
+    title: 'Task 1',
+    description: 'Example description!',
+    width: 200,
+    height: 100,
+    posX: 0,
+    posY: 100,
+    color: "#ff0000"
+  }
+]
+
 export default function Home() {
   return (
     <div style={{ background: 'white' }}>
       {/* <Task title="Hello" details="Details maybe" /> */}
       {/* <Task text="Task 2" details="More details" /> */}
+      <Task task={tasks[0]} />
       <h1 style={{ color: 'black' }}>Grid Canvas Example</h1>
       <GridCanvas />
     </div>
--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,14 +1,80 @@
-export default function Task(props: Task) {
-  let localText = props.title;
-  if (props.title == "Hello") {
-    localText = "Hello even more";
+"use client";
+import { useRef, useState } from 'react';
+import Moveable from 'react-moveable';
+
+interface Props {
+  task: Task;
+}
+
+export default function Task({ task }: Props) {
+  // const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+  // }
+  const target_ref = useRef(null);
+  const [task_state, setTaskState] = useState<Task>(task);
+  // Event handlers
+  const handleDrag = (e: any) => {
+    const { target, left, top } = e;
+    setTaskState({
+      ...task_state,
+      posX: left,
+      posY: top,
+    });
+    console.log("dragging", left, top)
+    console.log(e)
+  }
+  const handleDragEnd = (e: any) => {
+    const { target, left, top } = e;
+    setTaskState({
+      ...task_state,
+      posX: left,
+      posY: top,
+    });
+    console.log("dragging", left, top)
+    console.log(e)
+  }
+  const handleResize = (e: any) => {
+    const { target, clientX, clientY, width, height } = e;
+    // console.log(e);
+    setTaskState({
+      ...task_state,
+      // posX: clientX,
+      // posY: clientY,
+      width: width,
+      height: height
+    });
+    console.log("resizing", clientX, clientY, width, height)
   }
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col bg-red-600">
-        <p>{localText}</p>
-        <p>{props.description}</p>
+    <>
+      <div
+        className="h-[100px] w-[200px] select-none"
+        style={{
+          position: 'absolute',
+          left: task_state.posX,
+          top: task_state.posY,
+          width: task_state.width,
+          height: task_state.height,
+          background: task_state.color,
+          // transform: `translate(${task_state.posX}px, ${task_state.posY}px)`
+        }}
+        ref={target_ref}
+      >
+        <p>{task_state.title}</p>
       </div>
-    </div>
+      <Moveable
+        target={target_ref}
+        draggable={true}
+        resizable={true}
+        snappable={true}
+        snapThreshold={5}
+        snapDirections={['top', 'left', 'bottom', 'right']}
+        verticalGuidelines={[0,50,100,150,200,250,300,400,450,500,550]}
+        horizontalGuidelines={[0,50,100,150,200,250,300,400,450,500,550]}
+        throttleDrag={1}
+        throttleResize={1}
+        onDrag={handleDrag}
+        onResize={handleResize}
+      />
+    </>
   );
 }
