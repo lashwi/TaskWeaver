dd00e004698ee75ad7604a8b432e02a824a526c5
brine <cloneshipdom@gmail.com>
2023-11-10 10:59:52 -0600

    improve task details styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import React from "react";
 import GridCanvas from '@/components/Canvas';
 import Task from '@/components/Task';
+import TaskDetails from '@/components/TaskDetails';
 
 export default function Home() {
   return (
     <div style={{ background: 'white' }}>
       {/* <Task title="Hello" details="Details maybe" /> */}
       {/* <Task text="Task 2" details="More details" /> */}
+      {/*<TaskDetails />*/}
       <h1 style={{ color: 'black' }}>Grid Canvas Example</h1>
       <GridCanvas />
     </div>
--- client/src/components/TaskDetails.tsx
+++ client/src/components/TaskDetails.tsx
@@ -11,28 +11,28 @@ export default function TaskDetails({ onClose }: TaskPopupProps) {
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <div className={styles.closeButton} onClick={onClose}>
-          ✖️
-        </div>
         <div className={styles.panelContent}>
           <div className={styles.leftPanel}>
             <div className={styles.inputGroup}>
-              <input className={styles.inputField} placeholder="Untitled task" />
+              <input className={styles.inputFieldTitle} placeholder="Untitled task" />
             </div>
             <div className={styles.addButton}>Add Assignees +</div>
             <div className={styles.inputGroup}>
-              <textarea className={styles.inputField2} placeholder="Description"></textarea>
+              Description:
+              <textarea className={styles.inputField} placeholder="Description"></textarea>
             </div>
           </div>
 
           <div className={styles.rightPanel}>
             <div className={styles.dropdowns}>
-              <select className={styles.dropdown}><option>Status</option></select>
-              <select className={styles.dropdown}><option>Deadline</option></select>
-            </div>
-            <div className={styles.dropdowns}>
-              <select className={styles.dropdown}><option>Priority</option></select>
-              <select className={styles.dropdown}><option>Time Needed</option></select>
+              <div className={styles.dropdownColumn}>
+                <select className={styles.dropdown}><option>Status</option></select>
+                <select className={styles.dropdown}><option>Priority</option></select>
+              </div>
+              <div className={styles.dropdownColumn}>
+                <select className={styles.dropdown}><option>Deadline</option></select>
+                <select className={styles.dropdown}><option>Time Needed</option></select>
+              </div>
             </div>
             <div className={styles.inputGroup}> {/* Dropdowns go here */} </div>
             <div className={styles.addButton}>Add Task Dependency +</div>
@@ -43,8 +43,11 @@ export default function TaskDetails({ onClose }: TaskPopupProps) {
               View dependency graph
             </div>
           </div>
+
+          <div className={styles.closeButton} onClick={onClose}>
+            ✖️
+          </div>
         </div>
-        
       </div>
     </div>
   );
