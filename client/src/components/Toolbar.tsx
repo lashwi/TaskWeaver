c65ef41807c5302400db0f8f91543884a2579901
StephaniePatterson <stephanielpatter@gmail.com>
2023-11-13 23:20:11 -0600

    Toolbar with working buttons for changing cursors and basic css styling

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -1,24 +1,9 @@
 "use client";
-// import React from 'react';
-
-// export default function App(props) {
-//   return (
-//     <div style={{position:'fixed', left:0, top:100, display: 'flex', flexDirection: 'column'}}>
-//       <button className='button'>Select</button>
-//       <button className='button'>Move</button>
-//       <button className='button'>Task</button>
-//       <button className='button'>Arrow</button>
-//     </div>
-//   );
-// }
-
 import React from 'react';
 import { Button, IconButton, ButtonGroup, ButtonToolbar } from 'rsuite';
-import GearIcon from '@rsuite/icons/Gear';
-import MousePointer from '@rsuite/icons/legacy/MousePointer'
 import {ArrowFlowUpRight24Regular, CalendarAdd24Regular, ArrowMove24Regular, Cursor24Regular} from "@fluentui/react-icons"
-import Arrows from '@rsuite/icons/legacy/Arrows'
-import TaskPlus from '@rsuite/icons/legacy/ExpandO'
+
+import styles from './Toolbar.module.css'
 
 export default function App(props) {
   const changeCursor = (cursorType) => {
@@ -28,32 +13,34 @@ export default function App(props) {
     <div
       style={{
         position: 'fixed',
-        left: 0,
+        left: 1,
         top: 100,
         display: 'flex',
         flexDirection: 'column',
       }}
     >
-      <IconButton icon={<Cursor24Regular />} 
-        appearance='primary' 
-        color='cyan' 
-        onClick={() => changeCursor('default')}
-      />
-      <IconButton icon={<ArrowMove24Regular />} 
-        appearance='primary' 
-        color='blue' 
-        size='lg' 
-        onClick={() => changeCursor('move')}/>
-      <IconButton icon={<CalendarAdd24Regular />} 
-        appearance='primary'
-        color='blue'
-        size='lg'
-      />
-      <IconButton icon={<ArrowFlowUpRight24Regular />} 
-        appearance='primary'
-        color='blue'
-        size='lg'
-      />
+      <div className={styles.rectangle}>
+        <IconButton icon={<Cursor24Regular />} 
+          appearance='primary' 
+          color='cyan' 
+          onClick={() => changeCursor('default')}
+        />
+        <IconButton icon={<ArrowMove24Regular />} 
+          appearance='primary' 
+          color='blue' 
+          size='lg' 
+          onClick={() => changeCursor('move')}/>
+        <IconButton icon={<CalendarAdd24Regular />} 
+          appearance='primary'
+          color='blue'
+          size='lg'
+        />
+        <IconButton icon={<ArrowFlowUpRight24Regular />} 
+          appearance='primary'
+          color='blue'
+          size='lg'
+        />
+      </div>
     </div>
   );
 }
