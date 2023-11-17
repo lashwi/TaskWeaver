ca7460b82b798bc00d53a4117777f42efa4b5048
brine <cloneshipdom@gmail.com>
2023-11-17 10:07:40 -0600

    Small css changes

--- client/src/components/TaskDetails/DependencyView.tsx
+++ client/src/components/TaskDetails/DependencyView.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import styles from './TaskPopup.module.css';
+import CloseIcon from '@mui/icons-material/Close';
 
 type DependencyGraphPopupProps = {
   onClose: () => void; // function to close the popup
@@ -13,16 +14,7 @@ function DependencyView({ onClose }: DependencyGraphPopupProps) {
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <div className={styles.closeButton} onClick={onClose} style={{ position: 'relative' }}>
-          <span style={{
-            position: 'absolute',
-            top: '0',
-            right: '5px',
-            fontSize: '24px',
-            lineHeight: '24px',
-            cursor: 'pointer' // Optional, for a pointer cursor on hover
-          }}>×</span>
-        </div>
+      <CloseIcon className={styles.closeButton2} onClick={onClose}></CloseIcon>
         <div style={{ width: '50%', margin: '0px auto' }}>
           <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
             {task1.map((number, idx) =>
