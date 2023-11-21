7acca631cfc75549215bcfa09bfa2315fab21992
Minh Duong <git@whitehoodhacker.net>
2023-11-21 17:15:09 -0600

    Add back sample tasks

--- client/src/components/Board/index.tsx
+++ client/src/components/Board/index.tsx
@@ -18,27 +18,53 @@ export default function Board() {
   });
 
   const [tasks, setTaskList] = useState([
-      {
-        id: 1,
-        title: 'Assemble treehouse',
-        width: 200,
-        height: 100,
-        posX: 800,
-        posY: 250,
-        color: "#c9e4de"
-      }, 
-      {
-        id: 2,
-        title: 'Find wood',
-        width: 200,
-        height: 100,
-        posX: 400,
-        posY: 250,
-        color: "#c9e4de"
-    
-      }
+    {
+      id: 1,
+      title: 'Gather wood',
+      description: 'Example description!',
+      width: 200,
+      height: 100,
+      posX: 200,
+      posY: 350,
+      color: "#f7d9c4"
+    },
+    {
+      id: 2,
+      title: 'Build door',
+      width: 200,
+      height: 100,
+      posX: 500,
+      posY: 300,
+      color: "#faedcb"
+    },
+    {
+      id: 3,
+      title: 'Build walls',
+      width: 200,
+      height: 100,
+      posX: 500,
+      posY: 450,
+      color: "#faedcb"
+    },
+    {
+      id: 4,
+      title: 'Find cool tree',
+      width: 200,
+      height: 100,
+      posX: 300,
+      posY: 100,
+      color: "#f7d9c4"
+    },
+    {
+      id: 5,
+      title: 'Assemble treehouse',
+      width: 200,
+      height: 100,
+      posX: 800,
+      posY: 250,
+      color: "#c9e4de"
+    },
   ]);
-
 
   const handleAddTask = () => {
     var newTasks = tasks.slice();
@@ -50,12 +76,9 @@ export default function Board() {
       posX: 50,
       posY: 150,
       color: "#faedcb"
-      
-      });
-    console.log(newTasks.length);
+    });
     setTaskList(newTasks);
   };
-
 
   return (
     <div className="absolute top-0 left-0 z-10 h-screen w-screen overflow-hidden bg-white">
