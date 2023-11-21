603d53bd5e5eb4611bd0520da4688cf66c4cbc36
Minh Duong <git@whitehoodhacker.net>
2023-11-21 16:58:59 -0600

    Update toolbar styling

--- client/src/components/Toolbar/index.tsx
+++ client/src/components/Toolbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { Button, IconButton, ButtonGroup, ButtonToolbar } from 'rsuite';
+import { IconButton } from 'rsuite';
 import {
   ArrowFlowUpRight24Regular,
   ArrowMove24Regular,
@@ -8,17 +8,15 @@ import {
   Cursor24Regular
 } from "@fluentui/react-icons"
 
-import styles from './Toolbar.module.css';
-
 export default function Toolbar() {
   const changeCursor = (cursorType: string) => {
     document.body.style.cursor = cursorType;
   };
   return (
     <div
-      className="flex flex-col fixed h-screen left-0 top-[100px] z-30"
+      className="bg-surface-050/50 border-2 border-surface-100 rounded-xl backdrop-blur-2xl p-2 flex flex-col fixed left-4 top-64 z-30"
     >
-      <div className={styles.rectangle}>
+      <div className="grid grid-cols-1 gap-4">
         <IconButton icon={<Cursor24Regular />} 
           appearance='primary' 
           color='cyan' 
@@ -29,6 +27,7 @@ export default function Toolbar() {
           color='blue' 
           size='lg' 
           onClick={() => changeCursor('move')}/>
+        <hr className="border-surface-200" />
         <IconButton icon={<CalendarAdd24Regular />} 
           appearance='primary'
           color='blue'
