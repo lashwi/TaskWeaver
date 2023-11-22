1b2636dde099cf10f1add5517c8c415d1a17f5a0
lashwi <alamani2@illinois.edu>
2023-11-22 16:44:15 -0600

    add persona

--- client/src/components/Persona/Persona.tsx
+++ client/src/components/Persona/Persona.tsx
@@ -2,7 +2,7 @@ import './styles.css';
 export default function Persona() {
     return (
       <div className="pill">
-        <img src={'./facebook_icon.jpg'} alt="Image" className="pill-img"></img>
+        <img src={'./../../../facebook_icon.jpg'} alt="Image" className="pill-img"></img>
       </div>
     )
   }
