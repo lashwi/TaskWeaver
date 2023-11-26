48befe80d38c51de131b56dc33c2b11735e79ee4
Minh Duong <git@whitehoodhacker.net>
2023-11-26 12:58:54 -0600

    Add toolbar hover style

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -14,29 +14,31 @@ interface Props {
 
 export default function Toolbar({ selectedTool, setSelectedTool }: Props) {
   return (
-    <div className="bg-surface-050/50 border-2 border-surface-100 rounded-xl backdrop-blur-2xl p-2 flex flex-col fixed left-4 top-64 z-30">
-      <div className="grid grid-cols-1 gap-4">
+    <div className="bg-surface-050/50 border-2 border-surface-100 rounded-xl backdrop-blur-2xl flex flex-col fixed left-4 top-64 z-30">
+      <div className="grid grid-cols-1 gap-1 p-1">
         <button
-          className={selectedTool === Tool.Pointer ? 'bg-surface-150' : ''}
+          className={`p-1 rounded-lg hover:bg-surface-100 ${selectedTool === Tool.Pointer ? 'bg-surface-100' : ''}`}
           onClick={() => setSelectedTool(Tool.Pointer)}
         >
           <Cursor24Regular />
         </button>
         <button
-          className={selectedTool === Tool.Move ? 'bg-surface-150' : ''}
+          className={`p-1 rounded-lg hover:bg-surface-100 ${selectedTool === Tool.Move ? 'bg-surface-100' : ''}`}
           onClick={() => setSelectedTool(Tool.Move)}
         >
           <ArrowMove24Regular />
         </button>
-        <hr className="border-surface-200" />
+      </div>
+      <span className="border-t-2 border-surface-100" />
+      <div className="grid grid-cols-1 gap-1 p-1">
         <button
-          className={selectedTool === Tool.Task ? 'bg-surface-150' : ''}
+          className={`p-1 rounded-lg hover:bg-surface-100 ${selectedTool === Tool.Task ? 'bg-surface-100' : ''}`}
           onClick={() => setSelectedTool(Tool.Task)}
         >
           <SlideAdd24Regular />
         </button>
         <button
-          className={selectedTool === Tool.Arrow ? 'bg-surface-150' : ''}
+          className={`p-1 rounded-lg hover:bg-surface-100 ${selectedTool === Tool.Arrow ? 'bg-surface-100' : ''}`}
           onClick={() => setSelectedTool(Tool.Arrow)}
         >
           <ArrowFlowUpRight24Regular />
