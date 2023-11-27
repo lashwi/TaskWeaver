ca92708dcd5dfe10aee00fadaa00c1c9f8258062
Minh Duong <git@whitehoodhacker.net>
2023-11-26 18:05:20 -0600

    Use state from Board for Task and TaskDetailsPane

--- client/src/components/TaskDetailsPane/index.tsx
+++ client/src/components/TaskDetailsPane/index.tsx
@@ -1,67 +1,83 @@
-import { useState } from 'react';
-import Draggable from 'react-draggable';
-import './ResizablePane.css';
-import CloseIcon from '@mui/icons-material/Close';
+import { useRef, useState } from 'react';
+import Moveable from 'react-moveable';
+import Select from 'react-select';
+import { Dismiss24Filled } from '@fluentui/react-icons';
 import Dropdown from '@/components/Dropdown';
-import Select  from 'react-select';
-import { Button } from '@mui/joy';
 import DependencyView from '@/components/TaskDetails/DependencyView';
+import './ResizablePane.css';
 
-interface ResizablePaneProps {
-  isOpen: boolean;
-  onClose: () => void;
+interface Props {
   task: Task;
+  otherTasks: Task[];
+  arrows: Arrow[];
+  handleClose: () => void;
+  handleTaskUpdate: (task: Task) => void;
+};
 
-}
+interface PaneState {
+  posX: number;
+  posY: number;
+  width: number;
+  height: number;
+};
 
-export default function ResizablePane({ isOpen, onClose, task }: ResizablePaneProps) {
-  const [width, setWidth] = useState<number>(320); // Initial width of the pane
-  const [height, setHeight] = useState<number>(630); // Initial height of the pane
-  const [title, setTitle] = useState<string>('');
-  const [assignees, setAssignees] = useState<string[]>([]);
-  const [description, setDescription] = useState<string>('');
-
-  const handleResize = (event: React.MouseEvent) => {
-    // Calculate the new width and height based on mouse position
-    const newWidth = event.clientX - (event.currentTarget.getBoundingClientRect().left + window.scrollX);
-    const newHeight = event.clientY - (event.currentTarget.getBoundingClientRect().top + window.scrollY);
-
-    // Set minimum and maximum dimensions if needed
-    const minWidth = 200; // Minimum width in pixels
-    const maxWidth = 600; // Maximum width in pixels
-    const minHeight = 200; // Minimum height in pixels
-    const maxHeight = 600; // Maximum height in pixels
-
-    // Update the width and height within the specified limits
-    if (newWidth >= minWidth && newWidth <= maxWidth) {
-      setWidth(newWidth);
-    }
-    if (newHeight >= minHeight && newHeight <= maxHeight) {
-      setHeight(newHeight);
-    }
-  };
-  const handleResizeStop = (event: any, { size }: any) => {
-    // Update the width and height when resizing stops
-    setWidth(size.width);
-    setHeight(size.height);
-  };
-  
-  const other_tasks = [
-    {value: '1', label: 'Gather wood'},
-    {value: '2', label: 'Build door'}
-  ];
-
+export default function TaskDetailsPane(props: Props) {
+  const { task, otherTasks, arrows, handleClose, handleTaskUpdate } = props;
+  const target_ref = useRef(null);
+  const [pane_state, setPaneState] = useState<PaneState>({
+    posX: 0,
+    posY: 0,
+    width: 320,
+    height: 630
+  });
+  const [showDependencyGraph, setShowDependencyGraph] = useState(false);
   const personas = [
     {value: '1', label: 'Alice'},
     {value: '2', label: 'Bob'}
   ];
+  const taskOptions = otherTasks.map((t) => ({
+    value: t.id.toString(),
+    label: t.title
+  }));
 
-  const handleDescriptionChange = (des: string) => {
-    setDescription(des);
-  }
+  // Task update functions
+  const setTitle = (title: string) => {
+    handleTaskUpdate({
+      ...task,
+      title
+    });
+  };
+  const setDescription = (description: string) => {
+    handleTaskUpdate({
+      ...task,
+      description
+    });
+  };
 
-  // State to control whether the dependency graph popup is visible
-  const [showDependencyGraph, setShowDependencyGraph] = useState(false);
+  // const handleResize = (event: React.MouseEvent) => {
+  //   // Calculate the new width and height based on mouse position
+  //   const newWidth = event.clientX - (event.currentTarget.getBoundingClientRect().left + window.scrollX);
+  //   const newHeight = event.clientY - (event.currentTarget.getBoundingClientRect().top + window.scrollY);
+
+  //   // Set minimum and maximum dimensions if needed
+  //   const minWidth = 200; // Minimum width in pixels
+  //   const maxWidth = 600; // Maximum width in pixels
+  //   const minHeight = 200; // Minimum height in pixels
+  //   const maxHeight = 600; // Maximum height in pixels
+
+  //   // Update the width and height within the specified limits
+  //   if (newWidth >= minWidth && newWidth <= maxWidth) {
+  //     setPaneState
+  //   }
+  //   if (newHeight >= minHeight && newHeight <= maxHeight) {
+  //     setPaneHeight(newHeight);
+  //   }
+  // };
+  // const handleResizeStop = (event: any, { size }: any) => {
+  //   // Update the width and height when resizing stops
+  //   setPaneWidth(size.width);
+  //   setPaneHeight(size.height);
+  // };
 
   // Function to toggle the popup visibility
   const handleDependencyGraph = () => {
@@ -76,107 +92,120 @@ export default function ResizablePane({ isOpen, onClose, task }: ResizablePanePr
     // Implement logic to add assignees to the state
   };
 
-  const [task_state, setTaskState] = useState<Task>(task);
-
   return (
-    <div>
-      <Draggable handle=".pane-header" bounds="parent">
-          <div
-              className={`resizable-pane ${isOpen ? 'open' : ''}`}
-              style={{ width: `${width}px`, height: `${height}px` }}
-          >
-  
-          <div className="pane-header">
-            <h2></h2>
-            <CloseIcon className="closeButton2" onClick={onClose}></CloseIcon>
+    <span className="pointer-events-auto">
+      <div
+        className="resizable-pane open"
+        style={{
+          position: 'relative',
+          left: `${pane_state.posX}px`,
+          top: `${pane_state.posY}px`,
+          width: `${pane_state.width}px`,
+          height: `${pane_state.height}px`
+        }}
+        ref={target_ref}
+      >
+        <div className="pane-header">
+          <h2></h2>
+          {/* <CloseIcon className="closeButton2" onClick={onClose}></CloseIcon> */}
+          <button className="flex flex-row p-1 rounded-lg hover:bg-surface-100">
+            <Dismiss24Filled onClick={handleClose} />
+          </button>
+        </div>
+
+        <div className="assignee-section">
+          <div className="title">Title: </div>
+          <input
+            type="text"
+            placeholder="Untitled task"
+            defaultValue={task.title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="inputGroup" style={{top: '15px'}}>
+          <div style={{ display: 'flex', alignItems: 'center',top: '15px' }}>
+            <div style={{ marginRight: "6px" }}>Add Assignee:</div>
+            <div className="addButton">+</div>
           </div>
-  
-          <div className="assignee-section">
-            <div className="title">Title: </div>
-            <input type="text" placeholder="Title" defaultValue={task_state.title} onChange={(e) => setTitle(e.target.value)} />
-          </div>
-  
-          {/* <div style={{display:'flex', alignItems:'center'}}>
-            <div className="title">Add Assignee</div>
-            <button onClick={handleAddAssignee}> +</button>
-          </div> */}
-          <div className="inputGroup" style={{top: '15px'}}>
-              <div style={{ display: 'flex', alignItems: 'center',top: '15px' }}>
-                  <div style={{ marginRight: "6px" }}>Add Assignee:</div>
-                  <div className="addButton">+</div>
-              </div>
-              <Select
-                  isMulti
-                  name="colors"
-                  options={personas}
-                  className="basic-multi-select"
-                  classNamePrefix="select"
-              />
-          </div>
-  
-  
-          <div className="description-section">
-              Description:
-            <textarea
-              placeholder="Task Description"
-              // value={description}
-              defaultValue={task_state.description}
-              onChange={(e) =>handleDescriptionChange(e.target.value)}
+          <Select
+            isMulti
+            name="colors"
+            options={personas}
+            className="basic-multi-select"
+            classNamePrefix="select"
+          />
+        </div>
+        <div className="description-section">
+          <span>Description:</span>
+          <textarea
+            placeholder="Task Description"
+            defaultValue={task.description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className='flex flex-row w-full mb-4'>
+          <div className='grid grid-cols-2 gap-2 border w-full'>
+            <Dropdown 
+              options={['Status','Option2','Option3']}
+              onChange={handleDropdownChange}
+            />
+            <Dropdown 
+              options={['Deadline','Option2','Option3']}
+              onChange={handleDropdownChange}
+            />
+            <Dropdown 
+              options={['Priority','Option2','Option3']}
+              onChange={handleDropdownChange}
+            />
+            <Dropdown 
+              options={['Time Needed','Option2','Option3']}
+              onChange={handleDropdownChange}
             />
           </div>
-          <div className='flex flex-row w-full mb-4'>
-              <div className='grid grid-cols-2 gap-2 border w-full'>
-                  <Dropdown 
-                     options={['Status','Option2','Option3']}
-                     onChange={handleDropdownChange}
-                  />
-                  <Dropdown 
-                     options={['Deadline','Option2','Option3']}
-                     onChange={handleDropdownChange}
-                  />
-                  <Dropdown 
-                     options={['Priority','Option2','Option3']}
-                     onChange={handleDropdownChange}
-                  />
-                  <Dropdown 
-                     options={['Time Needed','Option2','Option3']}
-                     onChange={handleDropdownChange}
-                  />
-              </div>
-          </div>
-          <div className="inputGroup">
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <div style={{ marginRight: "6px" }}>This task depends on:</div>
-                  <div className="addButton">+</div>
-              </div>
-              <Select
-                  isMulti
-                  name="colors"
-                  options={other_tasks}
-                  className="basic-multi-select"
-                  classNamePrefix="select"
-              />
-          </div>
-          <div className= "inputGroup">
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <div style={{ marginRight: "6px" }}>Depends on this task:</div>
-                  <div className="addButton">+</div>
-              </div>
-              <Select
-                  isMulti
-                  name="colors"
-                  options={other_tasks}
-                  className="basic-multi-select"
-                  classNamePrefix="select"
-              />
-          </div>
-  
-          <div >
-              <Button onClick={handleDependencyGraph} className="bottomButton">View dependency graph</Button>
-          </div>
         </div>
-      </Draggable>
+        <div className="inputGroup">
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ marginRight: "6px" }}>This task depends on:</div>
+            <div className="addButton">+</div>
+          </div>
+          <Select
+            isMulti
+            name="colors"
+            options={taskOptions}
+            className="basic-multi-select"
+            classNamePrefix="select"
+          />
+        </div>
+        <div className= "inputGroup">
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ marginRight: "6px" }}>Depends on this task:</div>
+            <div className="addButton">+</div>
+          </div>
+          <Select
+            isMulti
+            name="colors"
+            options={taskOptions}
+            className="basic-multi-select"
+            classNamePrefix="select"
+          />
+        </div>
+        <div>
+          <button
+            className="bottomButton"
+            onClick={handleDependencyGraph}
+          >
+            View dependency graph
+          </button>
+        </div>
+      </div>
+      {/* <Moveable
+        target={target_ref}
+        draggable={true}
+        resizable={true}
+        throttleDrag={1}
+        throttleResize={1}
+      /> */}
       {showDependencyGraph && <DependencyView onClose={handleDependencyGraph} />}
-    </div>
+    </span>
   );
 };
