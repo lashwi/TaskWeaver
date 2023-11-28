f8cc94006812d174a5d2b18f3c5c55a87d031b36
brine <cloneshipdom@gmail.com>
2023-11-27 19:16:56 -0600

    re-add accidentally deleted changes

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -26,33 +26,45 @@ export default function Toolbar({ selectedTool, setSelectedTool }: Props) {
   return (
     <div className="bg-surface-050/50 border-2 border-surface-100 rounded-xl backdrop-blur-xl flex flex-col fixed left-4 top-64 z-30">
       <div className="grid grid-cols-1 gap-1 p-1">
-        <button
-          className={`p-1 rounded-lg hover:bg-surface-100 ${isPointer ? 'bg-surface-100' : ''}`}
-          onClick={() => setSelectedTool(Tool.Pointer)}
-        >
-          {isPointer ? <Cursor24Filled /> : <Cursor24Regular />}
-        </button>
-        <button
-          className={`p-1 rounded-lg hover:bg-surface-100 ${isMove ? 'bg-surface-100' : ''}`}
-          onClick={() => setSelectedTool(Tool.Move)}
-        >
-          {isMove ? <ArrowMove24Filled /> : <ArrowMove24Regular />}
-        </button>
+        <a data-tooltip-id='select' data-tooltip-content='Select' data-tooltip-place='right'>
+          <button
+            className={`p-1 rounded-lg hover:bg-surface-100 ${isPointer ? 'bg-surface-100' : ''}`}
+            onClick={() => setSelectedTool(Tool.Pointer)}
+          >
+            {isPointer ? <Cursor24Filled /> : <Cursor24Regular />}
+          </button>
+        </a>
+        <Tooltip id='select'/>
+        <a data-tooltip-id='move' data-tooltip-content='Move' data-tooltip-place='right'>
+          <button
+            className={`p-1 rounded-lg hover:bg-surface-100 ${isMove ? 'bg-surface-100' : ''}`}
+            onClick={() => setSelectedTool(Tool.Move)}
+          >
+            {isMove ? <ArrowMove24Filled /> : <ArrowMove24Regular />}
+          </button>
+        </a>
+        <Tooltip id='move'/>
       </div>
       <span className="border-t-2 border-surface-100" />
       <div className="grid grid-cols-1 gap-1 p-1">
-        <button
-          className={`p-1 rounded-lg hover:bg-surface-100 ${isTask ? 'bg-surface-100' : ''}`}
-          onClick={() => setSelectedTool(Tool.Task)}
-        >
-          {isTask ? <SlideAdd24Filled /> : <SlideAdd24Regular />}
-        </button>
-        <button
-          className={`p-1 rounded-lg hover:bg-surface-100 ${isArrow ? 'bg-surface-100' : ''}`}
-          onClick={() => setSelectedTool(Tool.Arrow)}
-        >
-          {isArrow ? <ArrowFlowUpRight24Filled /> : <ArrowFlowUpRight24Regular />}
-        </button>
+        <a data-tooltip-id='addtask' data-tooltip-content='Add Task' data-tooltip-place='right'>
+          <button
+            className={`p-1 rounded-lg hover:bg-surface-100 ${isTask ? 'bg-surface-100' : ''}`}
+            onClick={() => setSelectedTool(Tool.Task)}
+          >
+            {isTask ? <SlideAdd24Filled /> : <SlideAdd24Regular />}
+          </button>
+        </a>
+        <Tooltip id='addtask'/>
+        <a data-tooltip-id='addarrow' data-tooltip-content='Add Arrow' data-tooltip-place='right'>
+          <button
+            className={`p-1 rounded-lg hover:bg-surface-100 ${isArrow ? 'bg-surface-100' : ''}`}
+            onClick={() => setSelectedTool(Tool.Arrow)}
+          >
+            {isArrow ? <ArrowFlowUpRight24Filled /> : <ArrowFlowUpRight24Regular />}
+          </button>
+        </a>
+        <Tooltip id='addarrow'/>
       </div>
     </div>
   );
